4b859c97c6c1f131d6f4735595dd834197028958
taiseyReload <149210233+taiseyReload@users.noreply.github.com>
2024-06-13 16:25:11 -0300

    Update

--- admin/index.php
+++ admin/index.php
@@ -43,6 +43,7 @@ $config = $c->Listar();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Painel de controle</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
 
@@ -84,13 +85,15 @@ $config = $c->Listar();
     }
 
     .footer {
-        margin-top: 60px;
-        background: #010000;
-        padding-top: 90px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 10;
         text-align: center;
+        background-color: black;
         color: white;
-        border: none;
-        box-sizing: unset;
+        padding: 20px;
+        font-size: 15px;
     }
 
     /* Teste */
@@ -184,6 +187,15 @@ $config = $c->Listar();
 
                             </div>
                         </div>
+                        <ul>
+                                <br>
+                                <a href="../pagina_inicial/index.php" id="voltarpinicial" class="nav-link text-white">
+                                    <svg class="bi pe-none" width="16" height="16">
+                                        <use xlink:href="#"></use>
+                                    </svg>
+                                    <i class="bi bi-arrow-return-left"></i> Voltar a Página Inicial 
+                                </a>
+                            </ul>
                     </div>
             </nav>
 
@@ -493,17 +505,7 @@ $config = $c->Listar();
                 </div>
 
                 <!-- FOOTER -->
-                    <!-- <div class="footer">
-                        <div class="copyright">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <p>Copyright 2024. Todos os Direitos Reservados</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
+                <div class="footer">Copyright 2024. Todos os Direitos Reservados</div>
 
 
 
